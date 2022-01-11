16e98f5d3c2427db8ad519993bc84e542067cf97
Mikhail Krutov <mkrutov@redhat.com>
2022-01-11 13:55:15 +0100

    Add broker image update test to smoke suit

--- test/smoke/basic/basic_update_test.go
+++ test/smoke/basic/basic_update_test.go
@@ -2,16 +2,22 @@ package basic
 
 import (
 	"github.com/artemiscloud/activemq-artemis-operator-test-suite/pkg/bdw"
-	"github.com/artemiscloud/activemq-artemis-operator-test-suite/test"
 	"github.com/onsi/ginkgo"
 	"github.com/onsi/gomega"
 	"github.com/rh-messaging/shipshape/pkg/framework"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-var _ = ginkgo.Describe("DeploymentWithImageUpdates", func() {
+const (
+	CustomImage = "registry.redhat.io/amq7/amq-broker-rhel8@sha256:c5f4c08e068b9721967cf7c7cbd9a9e93fb5e39b264dd13b653e99d8f3fa9e0e"
+)
+
+var _ = ginkgo.Describe("DeploymentUpdateTests", func() {
 
 	var (
-		ctx1           *framework.ContextData
+		ctx1 *framework.ContextData
+		//brokerClient brokerclientset.Interface
 		brokerDeployer *bdw.BrokerDeploymentWrapper
 	)
 
@@ -22,19 +28,25 @@ var _ = ginkgo.Describe("DeploymentWithImageUpdates", func() {
 		setEnv(ctx1, brokerDeployer)
 	})
 
-	ginkgo.It("Deploy single broker, replace image with new one", func() {
-		gomega.Expect(brokerDeployer.DeployBrokers(1)).To(gomega.BeNil())
-		brokerDeployer.WithCustomImage(test.Config.BrokerImageOther)
-		gomega.Expect(brokerDeployer.ChangeImage()).To(gomega.BeNil())
-		gomega.Expect(brokerDeployer.VerifyImage(test.Config.BrokerImageOther)).NotTo(gomega.HaveOccurred())
+	ginkgo.It("CustomImageOverrideTest", func() {
+		brokerDeployer.WithCustomImage(CustomImage)
+		// TODO: extract this from operator.yaml
+		err := brokerDeployer.DeployBrokers(1)
+		gomega.Expect(err).To(gomega.BeNil(), "Broker deployment failed")
+		//TODO	// Also verify image from the ""broker"" instance
+		pod := getPod(ctx1)
+		actualImage := pod.Spec.Containers[0].Image
+		gomega.Expect(actualImage).To(gomega.Equal(CustomImage), "Image not updated after CR update")
+
 	})
 
-	ginkgo.It("Deploy single broker, scale down, replace image with new one, scale up", func() {
-		gomega.Expect(brokerDeployer.DeployBrokers(1)).To(gomega.BeNil())
-		gomega.Expect(brokerDeployer.Scale(0)).To(gomega.BeNil())
-		brokerDeployer.WithCustomImage(test.Config.BrokerImageOther)
-		gomega.Expect(brokerDeployer.ChangeImage()).To(gomega.BeNil())
-		gomega.Expect(brokerDeployer.Scale(1)).To(gomega.BeNil())
-		gomega.Expect(brokerDeployer.VerifyImage(test.Config.BrokerImageOther)).NotTo(gomega.HaveOccurred())
-	})
 })
+
+func getPod(ctx1 *framework.ContextData) *v1.Pod {
+	kubeclient := ctx1.Clients.KubeClient
+	podName := DeployName + PodNameSuffix
+	pod, err := kubeclient.CoreV1().Pods(ctx1.Namespace).Get(podName, metav1.GetOptions{})
+	gomega.Expect(err).To(gomega.BeNil())
+
+	return pod
+}
