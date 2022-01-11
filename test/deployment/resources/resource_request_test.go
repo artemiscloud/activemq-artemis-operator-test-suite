6552a7f82eb51efb3927cd37ce7c801066f09e91
Mikhail Krutov <mkrutov@redhat.com>
2022-01-11 13:55:44 +0100

    Add ResourceRequest modification test

--- test/deployment/resources/resource_request_test.go
+++ test/deployment/resources/resource_request_test.go
@@ -26,7 +26,7 @@ var _ = ginkgo.Describe("ResourceRequestsTests", func() {
 		deployBroker(brokerDeployer)
 		pod := getPod(ctx1)
 		actualCPURequest := pod.Spec.Containers[0].Resources.Requests.Cpu()
-		gomega.Expect(expectedCPURequest).To(gomega.Equal(actualCPURequest.String()))
+		gomega.Expect(expectedCPURequest).To(gomega.Equal(actualCPURequest.String()), "Expected CPU Request: %s, real: %s", expectedCPURequest, actualCPURequest.String())
 	})
 
 	ginkgo.It("Memory Request check", func() {
@@ -35,6 +35,23 @@ var _ = ginkgo.Describe("ResourceRequestsTests", func() {
 		deployBroker(brokerDeployer)
 		pod := getPod(ctx1)
 		actualMemRequest := pod.Spec.Containers[0].Resources.Requests.Memory()
-		gomega.Expect(expectedMemRequest).To(gomega.Equal(actualMemRequest.String()))
+		gomega.Expect(expectedMemRequest).To(gomega.Equal(actualMemRequest.String()), "Expected Memory limit: %s, real: %s", expectedMemLimit, actualMemLimit.String())
+	})
+
+	ginkgo.It("Memory Request check", func() {
+		expectedMemRequest := "512M"
+		brokerDeployer.WithMemRequest(expectedMemRequest)
+		deployBroker(brokerDeployer)
+		pod := getPod(ctx1)
+
+		actualMemRequest := pod.Spec.Containers[0].Resources.Requests.Memory()
+		gomega.Expect(expectedMemRequest).To(gomega.Equal(actualMemRequest.String()), "Expected Memory limit: %s, real: %s", expectedMemLimit, actualMemLimit.String())
+
+		expectedMemRequest = "768M"
+		brokerDeployer.WithMemRequest(expectedMemRequest)
+		brokerDeployer.Update()
+		actualMemRequest := pod.Spec.Containers[0].Resources.Requests.Memory()
+		gomega.Expect(expectedMemRequest).To(gomega.Equal(actualMemRequest.String()), "Expected Update to Memory limit: %s, real: %s", expectedMemLimit, actualMemLimit.String())
+
 	})
 })
