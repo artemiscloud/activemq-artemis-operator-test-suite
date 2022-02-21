af64e1c1b76969cf6569e63ebe9b55dea529c885
Mikhail Krutov <mkrutov@redhat.com>
2022-02-21 11:27:28 +0100

    +method to extract correct image from yaml

--- test/smoke/basic/basic_update_test.go
+++ test/smoke/basic/basic_update_test.go
@@ -2,15 +2,12 @@ package basic
 
 import (
 	"github.com/artemiscloud/activemq-artemis-operator-test-suite/pkg/bdw"
+	"github.com/artemiscloud/activemq-artemis-operator-test-suite/test"
 	"github.com/onsi/ginkgo"
 	"github.com/onsi/gomega"
 	"github.com/rh-messaging/shipshape/pkg/framework"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-)
-
-const (
-	CustomImage = "registry.redhat.io/amq7/amq-broker-rhel8@sha256:c5f4c08e068b9721967cf7c7cbd9a9e93fb5e39b264dd13b653e99d8f3fa9e0e"
 )
 
 var _ = ginkgo.Describe("DeploymentUpdateTests", func() {
@@ -29,6 +26,15 @@ var _ = ginkgo.Describe("DeploymentUpdateTests", func() {
 	})
 
 	ginkgo.It("CustomImageOverrideTest", func() {
+		images := test.GetImages()
+		imageName := decideImageName()
+		CustomImage := ""
+		for _, item := range images {
+			if item.Name == imageName {
+				CustomImage = item.Value
+				break
+			}
+		}
 		brokerDeployer.WithCustomImage(CustomImage)
 		// TODO: extract this from operator.yaml
 		err := brokerDeployer.DeployBrokers(1)
@@ -41,6 +47,19 @@ var _ = ginkgo.Describe("DeploymentUpdateTests", func() {
 	})
 
 })
+
+func decideImageName() string {
+	version := "_790"
+	name := "RELATED_IMAGE_ActiveMQ_Artemis_Broker_Kubernetes" + version
+	if test.Config.PPC {
+		name = name + "_ppc64le"
+	} else if test.Config.IBMz {
+		name = name + "_s390x"
+	} else {
+		//no need
+	}
+	return name
+}
 
 func getPod(ctx1 *framework.ContextData) *v1.Pod {
 	kubeclient := ctx1.Clients.KubeClient
